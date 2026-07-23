b5d5b6c9c7a0c1555b60c7a5251186820f18185a
macclary51-prog <macclary51@gmail.com>
2026-07-23 14:38:28 -0700

    Simplify Firebase config by removing unused services
    Removed Firestore and Storage imports and instances.

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,6 @@ import { initializeApp } from
 import { getAuth } from
     "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
 
-import { getFirestore } from
-    "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
-
-import { getStorage } from
-    "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
-
 
 const firebaseConfig = {
     apiKey: "AIzaSyDMWaronfPi0cujdvzGIsieadLss_d4iMQ",
@@ -22,15 +16,10 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
 
 
 export {
     app,
-    auth,
-    db,
-    storage
+    auth
 };
